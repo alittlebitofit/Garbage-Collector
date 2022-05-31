a3a643234d0e78fdbfe2317ca45c3ff09acbf45b
alittlebitofit <pam100@pm.me>
2022-06-01 00:20:20 +0530

    Notification working; next step is alarm

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.notification
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
+import android.app.Notification
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
+
 
 const val NOTIFICATION_ID = 1
 const val CHANNEL_ID = "channel id"
@@ -21,39 +18,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        createNotificationChannel()
+        val notificationReceiver = NotificationReceiver(this)
 
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+        notificationReceiver.notify(NOTIFICATION_ID, createNotification("Notification Title here"))
+
+    }
+
+
+
+    private fun createNotification(notificationTitle: String): Notification {
+
+        return NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_notifications_24)
-            .setContentTitle("textTitle")
+            .setContentTitle(notificationTitle)
             .setContentText("textContent")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        with(NotificationManagerCompat.from(this)) {
-            notify(NOTIFICATION_ID, builder.build())
-        }
-
-    }
-
-    private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = CHANNEL_NAME
-            val descriptionText = CHANNEL_DESCRIPTION
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-
-    private fun setAnAlarm() {
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH) // pops up the notification
+            .build()
     }
 
 }
--- app/src/main/java/com/example/notification/NotificationReceiver.kt
+++ app/src/main/java/com/example/notification/NotificationReceiver.kt
@@ -1,25 +1,85 @@
 package com.example.notification
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.Toast
+import androidx.core.app.NotificationManagerCompat
 
-class NotificationReceiver : BroadcastReceiver() {
+class NotificationReceiver(context: Context) : BroadcastReceiver() {
+
     override fun onReceive(context: Context?, intent: Intent?) {
 //        Toast.makeText(context, "Notification Receiver: onReceive()", Toast.LENGTH_LONG).show()
         intent?.let {
 
-            if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-                Toast.makeText(context, "Intent.ACTION_BOOT_COMPLETED", Toast.LENGTH_LONG).show()
-            }
+//            if (intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED || intent.action == Intent.ACTION_BOOT_COMPLETED) {
+//                Toast.makeText(context, "BOOT_COMPLETED", Toast.LENGTH_LONG)
+//                    .show()
+//
+//                context?.let {
+//                    createNotificationChannel(context)
+//                }
+//
+//            }
+
 
             if (intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED) {
-                Toast.makeText(context, "Intent.ACTION_LOCKED_BOOT_COMPLETED", Toast.LENGTH_LONG)
+                Toast.makeText(context, "ACTION_LOCKED_BOOT_COMPLETED", Toast.LENGTH_LONG)
                     .show()
+
+                    createNotificationChannel(localContext)
+
             }
+
+            if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+                Toast.makeText(context, "ACTION_BOOT_COMPLETED", Toast.LENGTH_LONG)
+                    .show()
+
+                    createNotificationChannel(localContext)
+
+            }
+
 
         }
 
     }
+
+    private val localContext: Context
+
+    init {
+        this.localContext = context
+        createNotificationChannel(localContext)
+    }
+
+    fun notify(notificationID: Int, notification: Notification) {
+        with(NotificationManagerCompat.from(localContext)) {
+            notify(notificationID, notification)
+            Toast.makeText(localContext, "notif popping", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+
+    private fun createNotificationChannel(context: Context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = CHANNEL_NAME
+            val descriptionText = CHANNEL_DESCRIPTION
+            val importance = NotificationManager.IMPORTANCE_HIGH // pops up the notification
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                context.getSystemService(NotificationManager::class.java)
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 }
